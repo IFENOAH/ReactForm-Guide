8b237086c091e1d52425dcfbfca15123762ad9a3
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-05-13 17:54:10 +0100

    refactored the list component to only render on user presence

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,17 +3,21 @@ import Card from "../UI/Card";
 import styles from "./UsersList.module.css";
 
 function UsersList(props) {
-  return (
-    <Card className={styles.users}>
-      <ul>
-        {props.users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.age} years old)
-          </li>
-        ))}
-      </ul>
-    </Card>
-  );
+  let content = "";
+  if (props.users.length) {
+    content = (
+      <Card className={styles.users}>
+        <ul>
+          {props.users.map((user) => (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          ))}
+        </ul>
+      </Card>
+    );
+  }
+  return <div>{content}</div>;
 }
 
 export default UsersList;
